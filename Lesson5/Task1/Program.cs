518bc8fcc1795160692ae33891ca1d89f4c81de9
Igor Bobkov <bobkovid@gmail.com>
2024-03-08 23:14:15 +0300

    Lesson5:Task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.Clear();
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1)!;
+}
+
+Console.WriteLine(Fact(5));
+
